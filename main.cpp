e4939ce229fef4171b294bd12448c6ae50f963a6
Bealang <104006409+Bealang@users.noreply.github.com>
2022-11-12 11:22:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ void otwieranie_sk()
 	char sel;
 	system("COLOR 0B");
 	system("cls");
-	cout<<" POBIERANIE SKRYPTOW"										<<endl;
+	cout<<" POBIERANIE SKRYPTOW"								<<endl;
 	cout<<" Wpisz numerek, odpowiadajacy skryptowi!"					<<endl;
-	cout<<"-------------------"											<<endl;
-	cout<<" 1 - Skrypt na ogloszenia"									<<endl;
-	cout<<" 2 - Skrypt na statystyki"									<<endl;
-	cout<<" 3 - Skrypt na afk"											<<endl;
-	cout<<" 4 - Skrypt na adminchat"									<<endl;
-	cout<<" 5 - Skrypt na chat on/off/clear"							<<endl;
-	cout<<" 6 - Skrypt na komendy po kliknieciu na tabliczke"			<<endl;
+	cout<<"-------------------"								<<endl;
+	cout<<" 1 - Skrypt na ogloszenia"							<<endl;
+	cout<<" 2 - Skrypt na statystyki"							<<endl;
+	cout<<" 3 - Skrypt na afk"								<<endl;
+	cout<<" 4 - Skrypt na adminchat"							<<endl;
+	cout<<" 5 - Skrypt na chat on/off/clear"						<<endl;
+	cout<<" 6 - Skrypt na komendy po kliknieciu na tabliczke"				<<endl;
 	cout<<" 7 - Skrypt na /repair i /repair all"						<<endl;
-	cout<<" 8 - Skrypt na znikanie blokow minute po postawieniu"		<<endl;
-	cout<<" -------------------"										<<endl;
-	cout<<" Wcisnij P, aby uzyskac pomoc"								<<endl;
+	cout<<" 8 - Skrypt na znikanie blokow minute po postawieniu"				<<endl;
+	cout<<" -------------------"								<<endl;
+	cout<<" Wcisnij P, aby uzyskac pomoc"							<<endl;
 	cout<<" Wybor: ";
 	sel = getch();
 	switch (sel)
@@ -28,14 +28,14 @@ void otwieranie_sk()
 		case 'P':
 		case 'p':
 			system ("cls");
-			cout<<"JAK WGRAC SKRYPT? PORADNIK"													         <<endl;
-			cout<<"----------------------------"													     <<endl;
-			cout<<"1. Pobierz Skript https://github.com/SkriptLang/Skript/releases"					     <<endl;
-			cout<<"2. Zrestartuj serwer i go wlacz"													     <<endl;
-			cout<<"3. Odswiez swoje FTP. (Zielona ikonka nad plikami w WinSCP)"						     <<endl;
-			cout<<"4. Otworz folder /plugins/skript/scripts/"										     <<endl;
-			cout<<"5. Stworz plik z rozszerzeniem .sk (np. rtp.sk)"									     <<endl;
-			cout<<"6. Otworz plik notatnikiem, notepad++, vsc czy innym edytorem tekstu"			     <<endl;
+			cout<<"JAK WGRAC SKRYPT? PORADNIK"							     <<endl;
+			cout<<"----------------------------"                                                         <<endl;
+			cout<<"1. Pobierz Skript https://github.com/SkriptLang/Skript/releases"			     <<endl;
+			cout<<"2. Zrestartuj serwer i go wlacz"							     <<endl;
+			cout<<"3. Odswiez swoje FTP. (Zielona ikonka nad plikami w WinSCP)"			     <<endl;
+			cout<<"4. Otworz folder /plugins/skript/scripts/"					     <<endl;
+			cout<<"5. Stworz plik z rozszerzeniem .sk (np. rtp.sk)"					     <<endl;
+			cout<<"6. Otworz plik notatnikiem, notepad++, vsc czy innym edytorem tekstu"		     <<endl;
 			cout<<"7. Wklej zawartosc skryptu lub stworz swoj (poradnik: https://youtu.be/xphlN2sI09Y)"  <<endl;
 			cout<<"8. Po wklejeniu kodu do pliku zapisz plik skrotem CTRL + S"                           <<endl;
 			cout<<"9. Przejdz do Minecrafta i wpisz komende: /sk reload plik.sk (np /sk reload rtp.sk)"  <<endl;
@@ -49,7 +49,7 @@ void otwieranie_sk()
 		case '3':    
 			ShellExecute(0, 0, "https://code.skript.pl/pGbRbwp5", 0, 0 ,   SW_SHOW );      break;
 		case '4':    
-			ShellExecute(0, 0, "https://pastebin.com/raw/P4uHH3kg", 0, 0 , SW_SHOW );    break;
+			ShellExecute(0, 0, "https://pastebin.com/raw/P4uHH3kg", 0, 0 , SW_SHOW );      break;
 		case '5':    
 			ShellExecute(0, 0, "https://code.skript.pl/qIw06wuC", 0, 0 ,   SW_SHOW );      break;
 		case '6':    
